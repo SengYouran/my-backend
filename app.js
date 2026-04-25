7947a70e1a63d9d220f7c1a9d353220efd9062a7
Youran <sengyouran2001@gmail.com>
2026-04-25 12:09:40 +0700

    fix​ change location cors

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 
-const app = express();
 const cors = require("cors");
+const app = express();
 const cookieParser = require("cookie-parser");
 
 // middleware
