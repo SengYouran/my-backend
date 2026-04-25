5ed52dfbc9b913c172c0a0af1683e2b5c0a28882
Youran <sengyouran2001@gmail.com>
2026-04-25 12:38:53 +0700

    add something mysql

--- db.js
+++ db.js
@@ -1,23 +1,11 @@
 const mysql = require("mysql2");
 
-const {
-  MYSQLHOST,
-  MYSQLUSER,
-  MYSQLPASSWORD,
-  MYSQLDATABASE,
-  MYSQLPORT,
-} = process.env;
-
-if (!MYSQLHOST || !MYSQLUSER || !MYSQLDATABASE) {
-  console.error("❌ Missing DB env variables");
-}
-
 const pool = mysql.createPool({
-  host: MYSQLHOST,
-  user: MYSQLUSER,
-  password: MYSQLPASSWORD,
-  database: MYSQLDATABASE,
-  port: MYSQLPORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
