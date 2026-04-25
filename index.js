ec20f15974cc3c4ffc289eb88da91148ed28b3fd
Youran <sengyouran2001@gmail.com>
2026-04-25 12:42:02 +0700

    add console.log

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 
 const app = require("./app");
+process.on("uncaughtException", (err) => {
+  console.log("🔥 CRASH:", err);
+});
 
+process.on("unhandledRejection", (err) => {
+  console.log("🔥 PROMISE ERROR:", err);
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
