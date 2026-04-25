67ba81acce4ffabfaf7f86779a6238f259f09e62
Youran <sengyouran2001@gmail.com>
2026-04-25 10:57:10 +0700

    fix mysql railway env

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
-// db.js
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Youran12#",
-  database: "project",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-// ✅ export pool.promise() directly
 module.exports = pool.promise();
