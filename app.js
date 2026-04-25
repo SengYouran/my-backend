3399cc612860e377ccd84b5d274e529bd539edda
Youran <sengyouran2001@gmail.com>
2026-04-25 12:27:40 +0700

    add something

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+// 🔥 1. PUT HERE (VERY TOP - BEFORE EVERYTHING)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://my-frontend-two-theta.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 // 🔥 MUST BE FIRST (IMPORTANT)
 app.use(cors({
   origin: "https://my-frontend-two-theta.vercel.app",
