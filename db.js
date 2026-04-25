9f9e564134ab2e9ecca52b498a29b92a9e7385eb
Youran <sengyouran2001@gmail.com>
2026-04-25 11:33:40 +0700

    fix mysql

--- db.js
+++ db.js
@@ -1,14 +1,23 @@
 const mysql = require("mysql2");
 
-if (!process.env.MYSQLHOST) {
-  throw new Error("MYSQLHOST missing");
+const {
+  MYSQLHOST,
+  MYSQLUSER,
+  MYSQLPASSWORD,
+  MYSQLDATABASE,
+  MYSQLPORT,
+} = process.env;
+
+if (!MYSQLHOST || !MYSQLUSER || !MYSQLDATABASE) {
+  console.error("❌ Missing DB env variables");
 }
+
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: MYSQLHOST,
+  user: MYSQLUSER,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE,
+  port: MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
