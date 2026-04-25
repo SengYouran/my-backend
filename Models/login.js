d4cd830bea627b388b0d8cce17d4a86d57a1a70a
Youran <sengyouran2001@gmail.com>
2026-04-25 20:11:42 +0700

    Update database

--- Models/login.js
+++ Models/login.js
@@ -2,9 +2,13 @@ const db = require("../db");
 
 const Login_ERP = {
   getUserByEmail: async (email) => {
+    console.log("🔥 SEARCH EMAIL:", email);
 
     const sql = "SELECT * FROM useraccount WHERE email = ?";
     const [rows] = await db.query(sql, [email]);
+
+    console.log("🔥 DB RESULT:", rows);
+
     return rows[0];
   },
 };
