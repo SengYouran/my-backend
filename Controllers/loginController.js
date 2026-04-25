1e56c0a25baeeb526be4c719d6dda7218151781a
Youran <sengyouran2001@gmail.com>
2026-04-25 18:29:12 +0700

    Update database

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -3,51 +3,36 @@ const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    console.log(email, password);
-    // 1️⃣ check input
-    if (!email || !password) {
-      return res.status(400).json({ message: "Email and password required" });
-    }
+    let { email, password } = req.body;
 
-    // 2️⃣ find user
-    const inputEmail = email.trim();
-    const user = await Login_ERP.getUserByEmail(inputEmail);
+    email = (email || "").trim();
+    password = (password || "").trim();
+
+    const user = await Login_ERP.getUserByEmail(email);
+
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
-    const inputPassword = password.trim();
 
-    if (inputPassword !== user.password.trim()) {
+    const dbPassword = String(user.password || "").trim();
+
+    console.log("INPUT:", password);
+    console.log("DB:", dbPassword);
+
+    if (password !== dbPassword) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    // 4️⃣ create JWT payload
-    const payload = {
-      employee_id: user.employee_id,
-      role: user.roles,
-    };
-
-    // 5️⃣ sign JWT
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
-
-    // 6️⃣ save JWT in HttpOnly cookie
-    res.cookie("token", token, {
-      httpOnly: true,
-      sameSite: "none",
-      secure: true, // true when HTTPS
-      maxAge: 24 * 60 * 60 * 1000,
-    });
-
-    // 7️⃣ response
-    res.status(200).json({
+    return res.status(200).json({
       message: "Login success",
+      user: {
+        id: user.employee_id,
+        role: user.roles,
+      },
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Server error" });
   }
 };
 
