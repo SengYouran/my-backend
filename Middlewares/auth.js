9b3b2985658182f4832ebc3dd7eaae21b526bf0e
Youran <sengyouran2001@gmail.com>
2026-04-27 12:53:37 +0700

    update cookie to heander

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,13 +1,12 @@
+// middlewares/auth.js
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const header = req.headers.authorization;
+  const token = req.cookies.token;
 
-  if (!header) {
+  if (!token) {
     return res.status(401).json({ message: "Not authenticated" });
   }
-
-  const token = header.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
